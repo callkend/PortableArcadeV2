84ba7606bd222aa7ea76e3a93787521f2382921e
Wyatt Callister <callwyat@isu.edu>
2022-07-10 16:20:23 -0700

    Created the PixiMatix.

---
+++ src/PixiPusher/PixiMatrix.c
@@ -0,0 +1,40 @@
+
+#include <stdint.h>
+#include "PixiMatrix.h"
+
+PixiMatrix PM_Init(uint16_t width, uint16_t height, uint8_t *pixelArray, const uint16_t *matrixMap) {
+    PixiMatrix result = {
+        .Width = width,
+        .Height = height,
+        .PixelArray = pixelArray,
+        .MatrixMap = matrixMap,
+    };
+
+    return result;
+}
+
+uint8_t* PM_GetLinerLocation(PixiMatrix matrix, uint16_t x, uint16_t y) {
+    
+    uint16_t linerLocation = y * matrix.Width + x;
+    return &matrix.PixelArray[(matrix.MatrixMap[linerLocation] * 3)];
+}
+
+void PM_SetPixel(PixiMatrix matrix, uint16_t x, uint16_t y, Color color) {
+    uint8_t *pnt = PM_GetLinerLocation(matrix, x, y);
+
+    *pnt++ = color.G;
+    *pnt++ = color.R;
+    *pnt = color.B;
+}
+
+Color PM_GetPixel(PixiMatrix matrix, uint16_t x, uint16_t y) {
+    uint8_t *pnt = PM_GetLinerLocation(matrix, x, y);
+
+    Color result = {
+        .G = *pnt++,
+        .R = *pnt++,
+        .B = *pnt++
+    };
+
+    return result;
+}
---
+++ src/PixiPusher/PixiMatrix.h
@@ -0,0 +1,37 @@
+/*
+ * File:   PixiMatrix.h
+ * Author: callwyat
+ *
+ * Created on July 10, 2022, 2:00 PM
+ */
+
+#ifndef PIXIMATRIX_H
+#define PIXIMATRIX_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdint.h>
+#include "color.h"
+
+    typedef struct PixiMatrix
+    {
+        uint16_t Width;
+        uint16_t Height;
+        uint8_t *PixelArray;
+        const uint16_t *MatrixMap;
+    } PixiMatrix;
+
+    PixiMatrix PM_Init(uint16_t width, uint16_t height, uint8_t *pixelArray, const uint16_t *matrixMap);
+
+    void PM_SetPixel(PixiMatrix matrix, uint16_t x, uint16_t y, Color color);
+
+    Color PM_GetPixel(PixiMatrix matrix, uint16_t x, uint16_t y);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PIXIMATRIX_H */
