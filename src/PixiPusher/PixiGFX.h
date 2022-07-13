577b858650d4e6aa682aa3bc8d32f31647dcb2f8
Wyatt Callister <callwyat@isu.edu>
2022-07-12 18:33:30 -0700

    Created the first font. Hopefully it is not too bad...

--- src/PixiPusher/PixiGFX.h
+++ src/PixiPusher/PixiGFX.h
@@ -12,22 +12,43 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+#include "Color.h"
+#include "PixiMatrix.h"
+    
+    typedef struct PGfxCharacter {
+        uint8_t Width;
+        uint8_t Rows[8];
+    } PGfxCharacter;
 
-Color PG_GetPixel(PixiMatrix matrix, uint16_t x, uint16_t y);
+#define DEFINE_Blank_PGfxChar(width) {      \
+    .Width = width,                         \
+    .Rows = {  }                            \
+}
+    
+#define DEFINE_PGfxChar(width, r0, r1, r2, r3, r4, r5, r6, r7) {  \
+    .Width = width,                                               \
+    .Rows = { r0, r1, r2, r3, r4, r5, r6 , r7 }                   \
+}
 
-void PG_SetPixel(PixiMatrix matrix, uint16_t x, uint16_t y, Color c);
-
-void PG_Fill(PixiMatrix matrix, Color c);
-
-void PG_DrawHorizontalLine(PixiMatrix matrix, uint16_t sx, uint16_t ex, uint16_t y, Color c);
-
-void PG_DrawVerticalLine(PixiMatrix matrix, uint16_t x, uint16_t sy, uint16_t ey, Color c);void PG_DrawRectangle(PixiMatrix matrix, uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, Color c);
-
-void PG_DrawRectangle(PixiMatrix matrix, uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, Color c);
-
-void PG_FillRectangle(PixiMatrix matrix, uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, Color c);
+    typedef struct PGfxFont {
+        PGfxCharacter Characters[128];
+    } PGfxFont;
 
 
+    Color PG_GetPixel(PixiMatrix matrix, uint16_t x, uint16_t y);
+
+    void PG_SetPixel(PixiMatrix matrix, uint16_t x, uint16_t y, Color c);
+
+    void PG_Fill(PixiMatrix matrix, Color c);
+
+    void PG_DrawHorizontalLine(PixiMatrix matrix, uint16_t sx, uint16_t ex, uint16_t y, Color c);
+
+    void PG_DrawVerticalLine(PixiMatrix matrix, uint16_t x, uint16_t sy, uint16_t ey, Color c);
+
+    void PG_DrawRectangle(PixiMatrix matrix, uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, Color c);
+
+    void PG_FillRectangle(PixiMatrix matrix, uint16_t sx, uint16_t sy, uint16_t ex, uint16_t ey, Color c);
 
 
 #ifdef	__cplusplus
