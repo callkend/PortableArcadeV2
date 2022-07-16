6ccac5c0fa928049f8c27556a606ce95e703fbc3
Wyatt Callister <callwyat@isu.edu>
2022-07-16 16:57:34 -0700

    Added array safety to Get and Set Pixel.

--- src/PixiPusher/PixiMatrix.c
+++ src/PixiPusher/PixiMatrix.c
@@ -15,26 +15,44 @@ PixiMatrix PM_Init(uint16_t width, uint16_t height, uint8_t *pixelArray, const u
 
 uint8_t* PM_GetLinerLocation(PixiMatrix* matrix, uint16_t x, uint16_t y) {
     
-    uint16_t linerLocation = y * matrix->Width + x;
-    return &matrix->PixelArray[(matrix->MatrixMap[linerLocation] * 3)];
+    // Make sure the pixel is rendered in a safe space;
+    if (x < matrix->Width && y < matrix->Height)
+    {
+        uint16_t linerLocation = y * matrix->Width + x;
+        return &matrix->PixelArray[(matrix->MatrixMap[linerLocation] * 3)];
+    }
+    else
+    {
+        return 0;
+    }
 }
 
 void PM_SetPixel(PixiMatrix* matrix, uint16_t x, uint16_t y, Color color) {
+
     uint8_t *pnt = PM_GetLinerLocation(matrix, x, y);
 
-    *pnt++ = color.G;
-    *pnt++ = color.R;
-    *pnt = color.B;
+    if (pnt)
+    {
+        *pnt++ = color.G;
+        *pnt++ = color.R;
+        *pnt = color.B;
+    }
 }
 
 Color PM_GetPixel(PixiMatrix* matrix, uint16_t x, uint16_t y) {
+
     uint8_t *pnt = PM_GetLinerLocation(matrix, x, y);
-
-    Color result = {
-        .G = *pnt++,
-        .R = *pnt++,
-        .B = *pnt++
-    };
-
-    return result;
+    if (pnt)
+    {
+        Color result = {
+            .G = *pnt++,
+            .R = *pnt++,
+            .B = *pnt++
+        };
+        return result;
+    }
+    else
+    {
+        return Black;
+    }
 }
